603289a339750baa3ae74853905321da96bdfa3b
Tomas Volka <tomas.volka@gmail.com>
2024-03-07 11:46:25 +0100

    things.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Awesome app
+
+## how to rock this ?
+- clone the repo
+- execute `docker-compose up`
+- have a coffee 
+- go to `127.0.0.1:8080`
+- enjoy 
+
+## what could be done better ?
+- probably everything, if i had more time
+- skipped the whole scrapy part, since they have nice jsons available
+- should have used different base docker image to avoid Postgres authentication pain
+- comments, docstrings, structuring, less chatgpt signature code, etc.
+
+Works on my Arch laptop. Thanks for attention, have a nice day.
--- app/main.py
+++ app/main.py
@@ -35,9 +35,16 @@ def index():
     return render_template('index.html', rows=rows)
 
 if __name__ == '__main__':
-    url = "https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&page=0&per_page=10"
+    url = "https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&page=0&per_page=500"
 
     print("Populating database")
+
+    conn = psycopg2.connect(
+        host='localhost',
+        user='myuser',
+        password='123',
+        database='mydatabase'
+    )
 
     # Make a GET request to the URL
     response = requests.get(url)
@@ -55,22 +62,15 @@ if __name__ == '__main__':
             image_url = (entry['_links']['images'][0].get('href'))
             print(f"{name} - {image_url}")
 
-            conn = psycopg2.connect(
-                host='localhost',
-                user='myuser',
-                password='123',
-                database='mydatabase'
-            )
-
             cur = conn.cursor()
 
             # Insert data into the table
             cur.execute("INSERT INTO mytable (name, image) VALUES (%s, %s)", (name, image_url))
 
-            # Commit the transaction and close the connection
-            conn.commit()
-            cur.close()
-            conn.close()
+        # Commit the transaction and close the connection
+        conn.commit()
+        cur.close()
+        conn.close()
 
 
     app.run(host='0.0.0.0', port=8080, debug=True)
